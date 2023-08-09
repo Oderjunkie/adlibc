5c7e779b9200d06eba897a878302afc038644835
Oderjunkie <otesunki@gmail.com>
2023-08-09 13:17:05 +0300

    add `open` and `close` syscalls

--- syscalls.c
+++ syscalls.c
@@ -8,6 +8,14 @@ extern ssize_t __write(int fd, const void *buf, size_t len) {
   return (ssize_t) __syscall(_WRITE_SYSCALL_NUM, fd, buf, len);
 }
 
+extern int __open(const char *filename, int flags, mode_t mode) {
+  return (int) (ssize_t) __syscall(_OPEN_SYSCALL_NUM, filename, flags, mode);
+}
+
+extern int __close(int fd) {
+  return (int) (ssize_t) __syscall(_CLOSE_SYSCALL_NUM, fd);
+}
+
 extern void *__mmap(void *addr, size_t len, int protections, int flags, int fd, off_t offset) {
   return (void *) __syscall(_MMAP_SYSCALL_NUM, addr, len, protections, flags, fd, offset);
 }
--- syscalls.h
+++ syscalls.h
@@ -5,6 +5,8 @@ extern void *__syscall(int, ...);
 enum {
   _READ_SYSCALL_NUM = 0,
   _WRITE_SYSCALL_NUM = 1,
+  _OPEN_SYSCALL_NUM = 2,
+  _CLOSE_SYSCALL_NUM = 3,
   _MMAP_SYSCALL_NUM = 9,
   _MUNMAP_SYSCALL_NUM = 11,
   _EXIT_SYSCALL_NUM = 60
@@ -12,11 +14,17 @@ enum {
 
 enum {
   PROT_READ = 0x01, PROT_WRITE = 0x02, PROT_EXEC = 0x04,
-  MAP_PRIVATE = 0x02, MAP_ANONYMOUS = 0x20
+  MAP_PRIVATE = 0x02, MAP_ANONYMOUS = 0x20,
+  O_RDONLY = 00, O_WRONLY = 01, O_RDWR = 02,
+  O_APPEND = 02000, O_CREAT = 0100, O_TRUNC = 01000,
+  S_IROTH = 0400
 };
 
 extern ssize_t __read(int, void *, size_t);
 extern ssize_t __write(int, const void *, size_t);
+extern int __open(const char *, int, mode_t);
+extern int __close(int);
 extern void *__mmap(void *, size_t, int, int, int, off_t);
 extern void __munmap(void *, size_t);
 extern void __exit(int);
+

7334c94f44db8c3041b7b57517d37735b7ea28ed
Oderjunkie <otesunki@gmail.com>
2023-08-09 13:12:45 +0300

    fix `fclose`'s declaration + change `getc`, `putc`, `getchar`, `putchar` to functions

--- include/stdio.h
+++ include/stdio.h
@@ -7,15 +7,15 @@ extern FILE *stdin;
 extern FILE *stdout;
 extern FILE *stderr;
 extern FILE *fopen(const char *, const char *);
-extern int fclose(const FILE *);
+extern int fclose(FILE *);
 extern size_t fwrite(const void *, size_t, size_t, FILE *);
 extern size_t fread(void *, size_t, size_t, FILE *);
 extern int fgetc(FILE *);
-#define getc(stream) fgetc(stream)
-#define getchar() getc(stdin)
+extern int getc(FILE *);
+extern int getchar(void);
 extern int fputc(int, FILE *);
-#define putc(ch, stream) fputc(ch, stream)
-#define putchar(ch) putc(ch, stdout)
+extern int putc(int, FILE *);
+extern int putchar(int);
 extern int feof(FILE *);
 extern int ferror(FILE *);
 extern int fputs(const char *, FILE *);
